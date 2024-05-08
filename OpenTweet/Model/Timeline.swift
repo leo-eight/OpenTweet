c0386636f87dde77940291654ce77d3c49733aab
leo-eight <moonbogi8@gmail.com>
2024-05-07 23:53:25 -0700

    Add Timeline model

---
+++ OpenTweet/Model/Timeline.swift
@@ -0,0 +1,11 @@
+//
+//  Timeline.swift
+//  OpenTweet
+//
+//  Created by Hyung Jip Moon on 2024-05-07.
+//  Copyright © 2024 OpenTable, Inc. All rights reserved.
+//
+
+struct Timeline: Decodable {
+    let timeline: [Tweet]
+}
